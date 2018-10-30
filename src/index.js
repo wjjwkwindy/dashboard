826d7443338ccecff3e8052b8bbe07d361cd79bd
Will Turner <willturner@lenovo.ubuntu>
2018-10-30 16:59:42 +0800

    feat: add second line in the main content
    1. Add second line in the main content.
    2. Add ECharts to show the data.
    3. Move @font-face to style.css.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './sass/style.scss';
 import './css/normalize.css';
 import './js/main.js';
+import './js/echrts-theme.js'
+import './js/echrts.js';
 
 console.log('[index.js] 这里是打包文件入口-index.js');
