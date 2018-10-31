4be56a0aeeca38466fda7150ad78b5a48364439f
wjjwkwindy <wjjwkwindy@gmail.com>
2018-10-31 11:38:19 +0800

    style: modify code style with Prettier

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,13 @@ let doc = document,
   weatherUrlBase = 'https://free-api.heweather.com/v5/weather',
   weatherKey = '987bc68871c94142ae815b39a1081e63',
   weatherCity = 'chengdu',
-  priceUrlBase='https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
-  priceKey='297fbec3-e209-40f1-9efa-1a067af4e3fe',
-  priceStart=1,
-  priceLimit=4,
-  priceConvert='CNY',
+  priceUrlBase =
+    'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+  priceKey = '297fbec3-e209-40f1-9efa-1a067af4e3fe',
+  priceStart = 1,
+  priceLimit = 4,
+  priceConvert = 'CNY',
+  newsUrl = 'http://c.m.163.com/nc/article/headline/T1348647853363/0-40.html',
   weekday = [
     'Sunday',
     'Monday',
@@ -36,13 +38,13 @@ let doc = document,
     yearProgressElem: doc.getElementById('yearProgress'),
     monthProgressElem: doc.getElementById('monthProgress'),
     weekProgressElem: doc.getElementById('weekProgress'),
-    USDPrice:doc.getElementById('USD-price'),
-    BTCPercent:doc.getElementById('BTC-percent'),
-    BCHPercent:doc.getElementById('BCH-percent'),
-    ETHPercent:doc.getElementById('ETH-percent'),
-    BTCPrice:doc.getElementById('BTC-price'),
-    BCHPrice:doc.getElementById('BCH-price'),
-    ETHPrice:doc.getElementById('ETH-price'),
+    USDPrice: doc.getElementById('USD-price'),
+    BTCPercent: doc.getElementById('BTC-percent'),
+    BCHPercent: doc.getElementById('BCH-percent'),
+    ETHPercent: doc.getElementById('ETH-percent'),
+    BTCPrice: doc.getElementById('BTC-price'),
+    BCHPrice: doc.getElementById('BCH-price'),
+    ETHPrice: doc.getElementById('ETH-price')
   };
 
 // 获取某个月份总天数
@@ -79,11 +81,11 @@ function getSessionStorage(key) {
 
 // 初始化请求
 function initialRequest(url) {
-  console.log('[info] '+url);
-  
+  console.log('[info] ' + url);
+
   return new Promise((resolve, reject) => {
     reject(new Error('故意的'));
-    
+
     let client = new XMLHttpRequest();
     client.open('GET', url);
     client.onreadystatechange = handler;
@@ -102,11 +104,14 @@ function initialRequest(url) {
       }
     }
   });
-  
 }
 // 初始化天气请求
 function initialWeatherRequest() {
-  const weatherUrl = addUrlParam(weatherUrlBase, ['city', 'key'], [weatherCity,weatherKey]);
+  const weatherUrl = addUrlParam(
+    weatherUrlBase,
+    ['city', 'key'],
+    [weatherCity, weatherKey]
+  );
   console.log('[info] ' + weatherUrl);
 
   return new Promise((resolve, reject) => {
@@ -133,7 +138,11 @@ function initialWeatherRequest() {
 
 // 处理天气请求
 let starWeatherRequest = function() {
-  const weatherUrl = addUrlParam(weatherUrlBase, ['city', 'key'], [weatherCity,weatherKey]);
+  const weatherUrl = addUrlParam(
+    weatherUrlBase,
+    ['city', 'key'],
+    [weatherCity, weatherKey]
+  );
   initialRequest(weatherUrl)
     .then(res => {
       console.log('[info] Get weather information succeed.');
@@ -143,7 +152,7 @@ let starWeatherRequest = function() {
     .catch(error => {
       console.log(error);
       console.log('[error] 重置上次请求时间为0');
-      sessionStorage.setItem('requestTime',0);
+      sessionStorage.setItem('requestTime', 0);
     });
 };
 
@@ -226,7 +235,11 @@ function handleWeather(res, origin = 'local') {
 
 // 初始化货币走势请求
 function initialPriceRequest() {
-  const priceUrl=addUrlParam(priceUrlBase,['CMC_PRO_API_KEY','start','limit','convert'],[priceKey,priceStart,priceLimit,priceConvert]);
+  const priceUrl = addUrlParam(
+    priceUrlBase,
+    ['CMC_PRO_API_KEY', 'start', 'limit', 'convert'],
+    [priceKey, priceStart, priceLimit, priceConvert]
+  );
   console.log('[info] ' + priceUrl);
 
   return new Promise((resolve, reject) => {
@@ -251,39 +264,55 @@ function initialPriceRequest() {
   });
 }
 // 获取货币走势
-(()=>{
-  const priceUrl=addUrlParam(priceUrlBase,['CMC_PRO_API_KEY','start','limit','convert'],[priceKey,priceStart,priceLimit,priceConvert]);
+(() => {
+  const priceUrl = addUrlParam(
+    priceUrlBase,
+    ['CMC_PRO_API_KEY', 'start', 'limit', 'convert'],
+    [priceKey, priceStart, priceLimit, priceConvert]
+  );
   initialRequest(priceUrl)
-    .then(res=>{
+    .then(res => {
       handlePrice(res);
     })
-    .catch(error=>{
+    .catch(error => {
       console.log(error);
-    })
+    });
 })();
 
 // 处理货币走势
 function handlePrice(res) {
-  let priceData=res.data,
+  let priceData = res.data,
     priceDataCache,
-    priceBTC=[],
-    priceETH=[],
-    priceBCH=[],
-    priceTotal=[];
+    priceBTC = [],
+    priceETH = [],
+    priceBCH = [],
+    priceTotal = [];
   for (let i = 0; i < priceData.length; i++) {
-    priceDataCache=priceData[i].quote.CNY;
+    priceDataCache = priceData[i].quote.CNY;
     switch (priceData[i].symbol) {
       case 'BTC':
         //priceBTC=[priceDataCache.price.toFixed(2),priceDataCache.percent_change_24h]
-        priceTotal[0]=['BTC',priceDataCache.price.toFixed(2),priceDataCache.percent_change_24h];
+        priceTotal[0] = [
+          'BTC',
+          priceDataCache.price.toFixed(2),
+          priceDataCache.percent_change_24h
+        ];
         break;
       case 'BCH':
         //priceBCH=[priceDataCache.price.toFixed(2),priceDataCache.percent_change_24h]
-        priceTotal[1]=['BCH',priceDataCache.price.toFixed(2),priceDataCache.percent_change_24h];
+        priceTotal[1] = [
+          'BCH',
+          priceDataCache.price.toFixed(2),
+          priceDataCache.percent_change_24h
+        ];
         break;
       case 'ETH':
         //priceETH=[priceDataCache.price.toFixed(2),priceDataCache.percent_change_24h]
-        priceTotal[2]=['ETH',priceDataCache.price.toFixed(2),priceDataCache.percent_change_24h];
+        priceTotal[2] = [
+          'ETH',
+          priceDataCache.price.toFixed(2),
+          priceDataCache.percent_change_24h
+        ];
         break;
       default:
         break;
@@ -293,60 +322,66 @@ function handlePrice(res) {
   for (let i = 0; i < priceTotal.length; i++) {
     switch (priceTotal[i][0]) {
       case 'BTC':
-        dataString.BTCPrice.innerText=priceTotal[i][1];
-        dataString.BTCPercent.innerText=priceTotal[i][2]+'%';
-        if(priceTotal[i][2]<0){
-          dataString.BTCPercent.previousElementSibling.classList.add('arrow-down');
+        dataString.BTCPrice.innerText = priceTotal[i][1];
+        dataString.BTCPercent.innerText = priceTotal[i][2] + '%';
+        if (priceTotal[i][2] < 0) {
+          dataString.BTCPercent.previousElementSibling.classList.add(
+            'arrow-down'
+          );
           dataString.BTCPercent.parentElement.classList.add('data-down');
-        }else{
+        } else {
           dataString.BTCPercent.parentElement.classList.add('data-up');
         }
         break;
 
       case 'BCH':
-        dataString.BCHPrice.innerText=priceTotal[i][1];
-        dataString.BCHPercent.innerText=priceTotal[i][2]+'%';
-        if(priceTotal[i][2]<0){
-          dataString.BCHPercent.previousElementSibling.classList.add('arrow-down');
+        dataString.BCHPrice.innerText = priceTotal[i][1];
+        dataString.BCHPercent.innerText = priceTotal[i][2] + '%';
+        if (priceTotal[i][2] < 0) {
+          dataString.BCHPercent.previousElementSibling.classList.add(
+            'arrow-down'
+          );
           dataString.BCHPercent.parentElement.classList.add('data-down');
-        }else{
+        } else {
           dataString.BCHPercent.parentElement.classList.add('data-up');
         }
         break;
 
       case 'ETH':
-        dataString.ETHPrice.innerText=priceTotal[i][1];
-        dataString.ETHPercent.innerText=priceTotal[i][2]+'%';
-        if(priceTotal[i][2]<0){
-          dataString.ETHPercent.previousElementSibling.classList.add('arrow-down');
+        dataString.ETHPrice.innerText = priceTotal[i][1];
+        dataString.ETHPercent.innerText = priceTotal[i][2] + '%';
+        if (priceTotal[i][2] < 0) {
+          dataString.ETHPercent.previousElementSibling.classList.add(
+            'arrow-down'
+          );
           dataString.ETHPercent.parentElement.classList.add('data-down');
-        }else{
+        } else {
           dataString.ETHPercent.parentElement.classList.add('data-up');
         }
         break;
       default:
         break;
-      }
-    
+    }
   }
 }
 
 // 获取美元汇率
-(()=>{
-  const usdPriceUrl = 'http://free.currencyconverterapi.com/api/v3/convert?q=USD_CNY&compact=ultra';
+(() => {
+  const usdPriceUrl =
+    'http://free.currencyconverterapi.com/api/v3/convert?q=USD_CNY&compact=ultra';
   initialRequest(usdPriceUrl)
-    .then(res=>{
+    .then(res => {
       handleUSDPrice(res);
     })
-    .catch(error=>{
-      console.log('[error] '+error);
-    })
-})()
+    .catch(error => {
+      console.log('[error] ' + error);
+    });
+})();
 
 // 处理美元汇率
 function handleUSDPrice(res) {
-  let usdPrice=res.USD_CNY.toFixed(2);
-  dataString.USDPrice.innerText=usdPrice;
+  let usdPrice = res.USD_CNY.toFixed(2);
+  dataString.USDPrice.innerText = usdPrice;
 }
 
 // 计算时间进度
