05a5ef45c7c9718aec995f04702fedce4071f573
wjjwkwindy <wjjwkwindy@gmail.com>
2018-12-01 17:17:46 +0800

    refactor: rename and move echarts files
    1. refactor: rename echarts file and move them to src/js/module/.
    2. fix: echarts filename in src/index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/style.scss';
 import './sass/col-refactor.scss';
 import './css/normalize.css';
 import './js/main.js';
-import './js/echrts-theme.js'
-import './js/echrts.js';
+import './js/module/echarts-theme.js'
+import './js/module/echarts.js';
 
 console.log('[index.js] 这里是打包文件入口-index.js');
