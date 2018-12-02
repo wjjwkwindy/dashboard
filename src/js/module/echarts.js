49724e3a2c27f7712743a16d26e2cc52706ba57b
wjjwkwindy <wjjwkwindy@gmail.com>
2018-12-02 10:26:50 +0800

    style: use Prettier to format code

--- src/js/module/echarts.js
+++ src/js/module/echarts.js
@@ -1,7 +1,13 @@
 // 最近7天浏览量
 var echarts = require("echarts");
-var viewIn7Days = echarts.init(document.getElementById("view_in_7days"),"walden");
-var viewByBrowers = echarts.init(document.getElementById("view_by_browers"),"walden");
+var viewIn7Days = echarts.init(
+  document.getElementById("view_in_7days"),
+  "walden"
+);
+var viewByBrowers = echarts.init(
+  document.getElementById("view_by_browers"),
+  "walden"
+);
 
 var viewIn7DaysOption = {
   title: {
@@ -11,18 +17,20 @@ var viewIn7DaysOption = {
   legend: {
     data: ["浏览量"]
   },
-xAxis: {
-    type: 'category',
+  xAxis: {
+    type: "category",
     data: ["7天前", "6天前", "5天前", "4天前", "3天前", "昨天", "今天"]
-},
-yAxis: {
-    type: 'value'
-},
-series: [{
-    data: [5, 4, 10, 25, 17, 32, 16],
-    type: 'line',
-    smooth: true
-}]
+  },
+  yAxis: {
+    type: "value"
+  },
+  series: [
+    {
+      data: [5, 4, 10, 25, 17, 32, 16],
+      type: "line",
+      smooth: true
+    }
+  ]
 };
 
 // 浏览量分类
@@ -33,48 +41,48 @@ var viewByBrowersOption = {
   //     x:'center'
   // },
   tooltip: {
-    trigger: 'item',
+    trigger: "item",
     formatter: "{a} <br/>{b}: {c} ({d}%)"
-},
-// legend: {
-//     orient: 'vertical',
-//     x: 'left',
-//     data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
-// },
-series: [
+  },
+  // legend: {
+  //     orient: 'vertical',
+  //     x: 'left',
+  //     data:['直接访问','邮件营销','联盟广告','视频广告','搜索引擎']
+  // },
+  series: [
     {
-        name:'访问来源',
-        type:'pie',
-        radius: ['60%', '70%'],
-        center: ['50%', '50%'],
-        avoidLabelOverlap: false,
-        label: {
-            normal: {
-                show: false,
-                position: 'center'
-            },
-            emphasis: {
-                show: true,
-                textStyle: {
-                    fontSize: '15',
-                    fontWeight: 'bold'
-                }
-            }
+      name: "访问来源",
+      type: "pie",
+      radius: ["60%", "70%"],
+      center: ["50%", "50%"],
+      avoidLabelOverlap: false,
+      label: {
+        normal: {
+          show: false,
+          position: "center"
         },
-        labelLine: {
-            normal: {
-                show: false
-            }
-        },
-        data:[
-            {value:30, name:'Chrome'},
-            {value:23, name:'Mobile'},
-            {value:15, name:'Firefox'},
-            {value:7, name:'Edge'},
-            {value:1, name:'IE'}
-        ]
+        emphasis: {
+          show: true,
+          textStyle: {
+            fontSize: "15",
+            fontWeight: "bold"
+          }
+        }
+      },
+      labelLine: {
+        normal: {
+          show: false
+        }
+      },
+      data: [
+        { value: 30, name: "Chrome" },
+        { value: 23, name: "Mobile" },
+        { value: 15, name: "Firefox" },
+        { value: 7, name: "Edge" },
+        { value: 1, name: "IE" }
+      ]
     }
-]
+  ]
 };
 
 viewIn7Days.setOption(viewIn7DaysOption);
